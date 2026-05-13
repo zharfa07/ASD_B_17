1aa89589fb3fbdcb9ea3577768165cf084bb019a
nabilasalsa3117-ctrl <nabilasalsa3117@gmail.com>
2026-05-13 15:02:50 +0700

    riwayat update.py

--- riwayat update.py
+++ riwayat update.py
@@ -1,8 +1,12 @@
-def tampilkan_stack():
-    print("\nRiwayat Update (Stack):")
+# riwayat.py
+from update import stack_update
+
+def tampilkan_riwayat():
+
+    print("\n=== Riwayat Update ===")
 
     if not stack_update:
-        print("Belum ada update.")
+        print("Belum ada riwayat.")
 
     else:
         for item in reversed(stack_update):
