2c5806b4f99f08c64502bf47ee0907ef8d53a722
nabilasalsa3117-ctrl <nabilasalsa3117@gmail.com>
2026-05-13 15:22:16 +0700

    Add files via upload

---
+++ tambah_data.py
@@ -0,0 +1,27 @@
+from config import jabatan_valid
+from file_handling import baca_data, simpan_data
+
+def tambah_data(stack):
+  print("\n-- Tambah Anggota --")
+  nama = input("Nama: ").strip()
+  if not nama or any(c.isdigit() for c in nama):
+    print("Nama tidak valid")
+    return
+
+  for i, j in enumerate(jabatan_valid, 1):
+    print(f" {i}. {j}")
+  pilihan = input("Nomor jabatan: ").strip()
+  if not pilihan.isdigit() or not (1 <= int(pilihan) <= len(jabatan_valid)):
+    print("Pilihan tidak valid")
+    return
+
+  jabatan = jabatan_valid[int(pilihan) - 1]
+  data = baca_data()
+  if jabatan != "Anggota" and any(d["Jabatan"] == jabatan for d in data):
+    print(f"Jabatan {jabatan} sudah ada")
+    return
+
+  data.append({"Nama": nama, "Jabatan": jabatan})
+  simpan_data(data)
+  stack.push(f"TAMBAH | {nama} | {jabatan}")
+  print(f"Berhasil menambahkan {nama} sebagai {jabatan}")
